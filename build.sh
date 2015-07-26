96e350c2266e192bec2eaa606000f9f8af3b1bda
nodiscc <nodiscc@gmail.com>
2015-07-26 17:06:58 +0200

    build.sh: add HTML <title> element for each page reads the title from the <!-- HTMLTITLE foo bar --> comment block in the source markdown file fixes https://github.com/RequestPolicyContinued/RequestPolicyContinued.github.io/issues/3

--- build.sh
+++ build.sh
@@ -7,14 +7,22 @@
 set -o errexit
 set -o nounset
 
+
+function gettitle() {
+	grep '<!-- HTMLTITLE' "$1" | \
+	sed -e 's/<!-- HTMLTITLE //g' -e 's/-->//g'
+}
+
 workdir="$(dirname $0)"
 cd $workdir
 mdfiles=$(find ./ -maxdepth 1 -name "*.md")
 
 for file in $mdfiles; do
+	htmltitle=$(gettitle "$file")
 	outfile="$(basename "$file" .md).html"
 	sed --file replace.sed < "$file" | \
     pandoc --from markdown_github \
+    --title-prefix="$htmltitle" \
     --write html5 \
     --standalone \
     --css "$(dirname $0)/github-markdown.css" \
